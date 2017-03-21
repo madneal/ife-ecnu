59cb06a7d81cfce57875451cb90087229981a7f7
neal1991 <neal1991@sina.com>
2017-03-21 12:30:32 +0800

    move ife2017 here

---
+++ nuomi/web-parser/task1.js
@@ -0,0 +1,76 @@
+/**
+ * Created by neal1 on 2017/3/6.
+ */
+var webPage = require('webpage');
+var page = webPage.create();
+var fs = require('fs');
+phantom.outputEncoding="gb2312"
+
+// in order to display the console info
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+page.open('http://www.baidu.com', function(status) {
+    var searchWord = '12306';
+    var beginTime = Date.now();
+    page.evaluate(function() {
+        document.querySelector('#kw').value = '12306';
+        console.log(document.querySelector('#kw').value);
+        document.querySelector('#su').click();
+        console.log(document.title);
+        return document.title;
+    })
+    setTimeout(function() {
+        page.evaluate(function(beginTime) {
+        var result = {};
+        console.log(document.title);
+        console.log('the status is ' + status);
+        result.code = 1;
+        result.msg = '抓取成功';
+        var endTime = Date.now();
+        console.log('the begin time is: ' + beginTime);
+        console.log('the end time is: ' + endTime);
+        var loadTime = endTime - beginTime;
+        result.time = loadTime;
+        result.dataList = [];
+        console.log('the document title ' + document.title);
+        var dataListDoms = document.querySelectorAll('#content_left .result-op');
+        console.log('the length of dataListDoms is ' + dataListDoms.length);
+        if (!dataListDoms) {
+            console.log('the dataListDoms is empty');
+        } else {            
+            console.dir(dataListDoms);
+        }
+        for (var i = 0; i < dataListDoms.length; i++) {
+            var ele = {};
+            var dom = dataListDoms[i];
+            var title = dom.querySelector('h3 a');
+            var pic = dom.querySelector('img');
+            var info = dom.querySelector('.c-span21');
+            if (title) {
+                ele.title = title.textContent;
+                ele.link = title.href;
+                console.log('ele title ' + ele.title);
+                console.log('ele link ' + ele.link);
+            }
+            if (pic) {
+                ele.pic = pic.src;
+            }
+            if (info) {
+                ele.info = info.childNodes[1].textContent;
+                console.log('ele info ' + ele.info);
+            }
+            result.dataList.push(ele);
+        }
+        console.log(JSON.stringify(result));
+        })
+    }, 10000)
+    setTimeout(function() {
+        phantom.exit();
+    }, 20000)
+  // phantom.exit();
+});
+
+
+
